bb075c3f2c77af81b37c28739bcffe1d79927740
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-20 22:30:35 -0300

    chore: update shareCode to 5K4VAITFCMN

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/fowsuqio?shareCode=HKGZTQ0V3U5&afp=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/fowsuqio?shareCode=5K4VAITFCMN&afp=tapi2' });
     res.end();
   }
