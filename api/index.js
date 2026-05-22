e9ee18f7e12b1069b1bb2782b2418035e7837f40
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-22 08:43:46 -0300

    chore: update shareCode to WX6QDGEZY1E

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/fowsuqio?shareCode=5K4VAITFCMN&afp=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/fowsuqio?shareCode=WX6QDGEZY1E&afp=tapi2' });
     res.end();
   }
