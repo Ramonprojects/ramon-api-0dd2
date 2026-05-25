00f3cb2dd8442f9c51cf9896c630ff644621d48d
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-25 16:01:20 -0300

    chore: update shareCode to 62SPGFXPSAE

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/fowsuqio?shareCode=3EY6APILP42&afp=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/fowsuqio?shareCode=62SPGFXPSAE&afp=tapi2' });
     res.end();
   }
