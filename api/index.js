48ff5b4c043e57eb3d1c0f7d299cdb3dc62d94a9
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-25 21:13:29 -0300

    chore: update shareCode to S4U910T1MS6

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/fowsuqio?shareCode=62SPGFXPSAE&afp=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/fowsuqio?shareCode=S4U910T1MS6&afp=tapi2' });
     res.end();
   }
