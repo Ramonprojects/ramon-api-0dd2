db8717ea3ebbbefce4183662bcdf9715783ba2f1
Ramonprojects <adm@ramonmago.com>
2026-06-03 20:29:10 -0300

    chore: shareCode C4XD5GHVCRH -> A04JGKJ2MPB (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/fowsuqio?shareCode=C4XD5GHVCRH&afp=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/fowsuqio?shareCode=A04JGKJ2MPB&afp=tapi2' });
     res.end();
   }
