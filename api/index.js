af40250915b8970f61c830d209acadf6f6980991
Ramonprojects <adm@ramonmago.com>
2026-06-07 22:14:41 -0300

    chore: shareCode MPZC2COIR8O -> W7YZQASPMV4 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/fowsuqio?shareCode=MPZC2COIR8O&afp=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/fowsuqio?shareCode=W7YZQASPMV4&afp=tapi2' });
     res.end();
   }
