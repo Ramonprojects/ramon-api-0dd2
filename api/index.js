5bbabb591261edfec2d4348b6922c1d3a791863b
Ramonprojects <adm@ramonmago.com>
2026-06-23 20:21:32 -0300

    chore: shareCode V9CG1PKCBVO -> 979QEGI2FJC (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=24737'
 
-    const shareCode = '&shareCode=V9CG1PKCBVO'
+    const shareCode = '&shareCode=979QEGI2FJC'
     
     const afp = '&afp2=tapi2'
     
