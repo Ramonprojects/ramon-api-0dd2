0a77fae4999fac39c082753d2aedc11a68df213f
Ramonprojects <adm@ramonmago.com>
2026-06-25 23:41:02 -0300

    chore: shareCode JEYCIIL10SV -> IS4L9OTXEV0 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=24737'
 
-    const shareCode = '&shareCode=JEYCIIL10SV'
+    const shareCode = '&shareCode=IS4L9OTXEV0'
     
     const afp = '&afp2=tapi2'
     
