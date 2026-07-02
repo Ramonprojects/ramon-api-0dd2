e0ce1dae8aa81846d28eb913a7aa974def4c80ec
Ramonprojects <adm@ramonmago.com>
2026-07-01 22:37:15 -0300

    chore: shareCode KLX7O1WIS8C -> DN0ED7TBVEV (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252'
 
-    const shareCode = '&shareCode=KLX7O1WIS8C'
+    const shareCode = '&shareCode=DN0ED7TBVEV'
     
     const afp = '&afp6=tapi2'
     
