66ae2eeeab6a71ecff18df229a401cc73ab37704
Ramonprojects <adm@ramonmago.com>
2026-07-07 00:20:48 -0300

    chore: shareCode HTUKWDIJDYR -> UVKF2TMWKF6 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252'
 
-    const shareCode = '&shareCode=HTUKWDIJDYR'
+    const shareCode = '&shareCode=UVKF2TMWKF6'
     
     const afp = '&afp6=tapi2'
     
